024bc36ee0efb82a8d4bf60b6d413c0e3721939a
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2024-02-06 11:30:11 +0200

    add build and install scripts

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eo pipefail
+
+# Install the Rust binaries to path.
+
+# Import common functions
+DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=./common.sh
+source "$DIR/common.sh"
+
+if [ -z "$(command -v cargo)" ]; then
+    print_error_and_exit "Cargo not found in path. Maybe install rustup?"
+fi
+
+cargo install --path "$REPO_ROOT"
+
+executables=$(get_rust_executable_names)
+for executable in $executables; do
+    if [ -z "$(command -v "$executable")" ]; then
+        print_error_and_exit "Binary not found. Is the Cargo install directory in path?"
+    fi
+    echo "$($executable --version) from $(which "$executable")"
+done
