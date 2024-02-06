ece0a46b775420872326482dae142928df2d97c8
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2024-02-06 11:50:41 +0200

    improvements to dots

--- src/bin/dots.rs
+++ src/bin/dots.rs
@@ -3,11 +3,11 @@ extern crate colored;
 use anyhow::Result;
 use clap::Parser;
 use colored::Colorize;
+use walkdir::WalkDir;
 
 use std::fs;
 use std::path::Path;
 use std::path::PathBuf;
-use walkdir::WalkDir;
 
 #[derive(Parser, Debug)]
 #[command(
@@ -20,11 +20,11 @@ struct Args {
     /// Optional input directory or file
     input_dir: String,
 
-    /// Do not ask for confirmation
+    /// Overwrite existing files
     #[arg(short, long)]
     force: bool,
 
-    /// Only print changes
+    /// Only print changes without renaming
     #[arg(short, long)]
     print: bool,
 
@@ -33,8 +33,20 @@ struct Args {
     verbose: bool,
 }
 
-fn replace_whitespaces<P: Into<PathBuf>>(path: P) -> Result<()> {
+fn replace_whitespaces<P: Into<PathBuf>>(
+    path: P,
+    dryrun: bool,
+    overwrite: bool,
+    verbose: bool,
+) -> Result<()> {
     let path = path.into();
+    let mut num_renamed: usize = 0;
+    if verbose {
+        println!(
+            "{}",
+            format!("Formatting files under {}", path.display()).bold()
+        )
+    }
     for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
         let path = entry.path();
         if path.is_file() {
@@ -42,15 +54,37 @@ fn replace_whitespaces<P: Into<PathBuf>>(path: P) -> Result<()> {
                 if file_name.contains(' ') {
                     let new_file_name = file_name.replace(' ', ".");
                     let new_path = path.with_file_name(new_file_name);
-                    match fs::rename(path, &new_path) {
-                        Ok(_) => println!("Renamed {} to {}", path.display(), new_path.display()),
-                        Err(e) => {
-                            eprintln!("{}", format!("Error renaming {:?}: {}", path, e).red())
+                    if dryrun {
+                        println!("Dryrun: {} to {}", path.display(), new_path.display());
+                        num_renamed += 1;
+                    } else if new_path.exists() && !overwrite {
+                        println!(
+                            "{}",
+                            format!(
+                                "Skipping rename to already existing file: {}",
+                                new_path.display()
+                            )
+                            .yellow()
+                        )
+                    } else {
+                        match fs::rename(path, &new_path) {
+                            Ok(_) => {
+                                println!("Renamed {} to {}", path.display(), new_path.display());
+                                num_renamed += 1;
+                            }
+                            Err(e) => {
+                                eprintln!("{}", format!("Error renaming {:?}: {}", path, e).red());
+                            }
                         }
                     }
                 }
             }
         }
+    }
+    if dryrun {
+        println!("Dryrun: would have renamed {} files", num_renamed);
+    } else {
+        println!("{}", format!("Renamed {} files", num_renamed).green());
     }
     Ok(())
 }
@@ -69,5 +103,5 @@ fn main() -> Result<()> {
         );
     }
     let absolute_input_path = fs::canonicalize(filepath)?;
-    replace_whitespaces(absolute_input_path)
+    replace_whitespaces(absolute_input_path, args.print, args.force, args.verbose)
 }
